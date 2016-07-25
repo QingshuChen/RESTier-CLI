060f636bb24bc0e409d763771d49d0a5aefd4f03
Qingshu Chen <qingshu.chen714@gmail.com>
2016-07-25 17:56:31 +0800

    add implementation for Build and Run command

--- RESTier.CLI/Commands/BuildCommand.cs
+++ RESTier.CLI/Commands/BuildCommand.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Microsoft.Extensions.CommandLineUtils;
+using System.Diagnostics;
+using System.IO;
 
 namespace Microsoft.RESTier.Cli.Commands
 {
@@ -10,13 +12,73 @@ namespace Microsoft.RESTier.Cli.Commands
             command.Description = "Builds the RESTier project.";
             command.HelpOption("--help");
 
-            command.Option("-p|--project", "The name for the RESTier project", CommandOptionType.SingleValue);
+            var projectName = command.Option("-p|--project", "The name for the RESTier project", CommandOptionType.SingleValue);
+            var buildSetting = command.Option("-s|--build-setting",
+               "Parameter for the msbuild when complile the project",
+               CommandOptionType.SingleValue);
 
             command.OnExecute(() =>
             {
+                string pName = "";
+                if (string.IsNullOrEmpty(projectName.Value()))
+                {
+                    string dir = Directory.GetCurrentDirectory();
+                    DirectoryInfo sDir = new DirectoryInfo(dir);
+                    FileInfo[] fileArray = sDir.GetFiles();
+                    foreach (FileInfo file in fileArray)
+                    {
+                        if (file.Extension.Equals("sln"))
+                        {
+                            pName = file.Name;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    pName = projectName.Value();
+                }
+
+                if (pName.Length == 0)
+                {
+                    Console.Write("Can't find a project to build");
+                    command.ShowHelp();
+                    return 0;
+                }
+
+                CmdMSBuild(pName, buildSetting.Value());
+
                 Console.WriteLine("Built {0} successfully.", command.GetOptionValue("p"));
                 return 0;
             });
+        }
+
+        // execute the msbuild to build a project
+        private static void CmdMSBuild(string projectName, string buildSetting)
+        {
+
+            Process p = new Process();
+
+            p.StartInfo.FileName = "cmd.exe";
+
+            p.StartInfo.UseShellExecute = false;
+
+            p.StartInfo.Arguments = "/c " + Config.MSBuildPath + "MSBuild.exe " + projectName +
+                (string.IsNullOrEmpty(buildSetting) ? "" : " " + buildSetting);
+
+            p.Start();
+
+            p.WaitForExit();
+        }
+
+        // Ignore the last line that contains the word "exit"
+        // Used to delete the output when execute "exit" command
+        private static string ignoreLastLineContainWordExit(string str)
+        {
+            int index = str.LastIndexOf('\n', str.LastIndexOf("exit"));
+            if (index < 0) // unlikely to happen
+                return str;
+            return str.Substring(0, index);
         }
     }
 }
--- RESTier.CLI/Commands/RunCommand.cs
+++ RESTier.CLI/Commands/RunCommand.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Microsoft.Extensions.CommandLineUtils;
+using System.Diagnostics;
+using System.IO;
 
 namespace Microsoft.RESTier.Cli.Commands
 {
@@ -13,11 +15,51 @@ namespace Microsoft.RESTier.Cli.Commands
                 CommandOptionType.SingleValue);
             command.Option("-p|--project", "The name for the RESTier project", CommandOptionType.SingleValue);
 
+            CommandOption projectDir = command.Option("-d|--project-directory",
+               " The directory that contains the project to run, default to current directory",
+               CommandOptionType.SingleValue);
+
             command.OnExecute(() =>
             {
-                Console.WriteLine("API is hosted at http://localhost:8080.");
+                //    Console.WriteLine("API is hosted at http://localhost:8080.");
+                string pDir = "";
+                if (string.IsNullOrEmpty(projectDir.Value()))
+                {
+                    pDir = Directory.GetCurrentDirectory();
+
+                }
+                else
+                {
+                    pDir = projectDir.Value();
+                }
+                if (!File.Exists(pDir + "\\" + ".vs\\config\\applicationhost.config"))
+                {
+                    Console.Write("Can't find the configration file '" + pDir + "\\" + ".vs\\config\\applicationhost.config' \n" +
+                        "Make sure you have set the correct project directory");
+                    command.ShowHelp();
+                    return 0;
+                }
+
+                CmdIISExpress(pDir);
+
                 return 0;
             });
+        }
+
+        // execute the msbuild to build a project
+        private static void CmdIISExpress(string projectDir)
+        {
+
+            Process p = new Process();
+
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.Arguments = "/c \"" + Config.IISExpressPath + "iisexpress.exe\" " +
+                 "/config:" + projectDir + "\\" + ".vs\\config\\applicationhost.config";
+            Console.WriteLine(p.StartInfo.Arguments);
+            p.StartInfo.UseShellExecute = false;
+            p.Start();
+            p.WaitForExit();
+
         }
     }
 }
